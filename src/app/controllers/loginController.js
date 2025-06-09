24a52a40fd7c4d8fd43382a0c2183dc7014d1c2f
viethoang1520 <viethoang1520.dev@gmail.com>
2025-06-09 11:32:37 +0700

    fix: login

--- src/app/controllers/loginController.js
+++ src/app/controllers/loginController.js
@@ -5,16 +5,18 @@ const loginUser = async (req, res) => {
   const { username, password } = req.body
   const user = await User.findOne({ username })
   if (!user) {
-    res.status(404).json({"message": "User has not been registed yet"})
+    return res.status(404).json({ "message": "User has not been registed yet" })
+    
   }
-  console.log(user)
-  const validUser = bcrypt.compare(password, user.password_hash)
+
+  const validUser = await bcrypt.compare(password, user.password_hash)
+
   if (validUser) {
-    const token = jwt.sign({id: user._id, username}, process.env.JWT_SECRET)
-    res.status(200).json({"message": "User logged in !!", token})
+    const token = jwt.sign({ id: user._issd, username }, process.env.JWT_SECRET)
+    return res.status(200).json({ "message": "User logged in !!", token })
   } else {
-    res.status(500).json({"message": "Incorrect password"})
+    return res.status(500).json({ "message": "Incorrect password" })
   }
 }
 
-module.exports = {loginUser}
+module.exports = { loginUser }
--- src/app/controllers/registerController.js
+++ src/app/controllers/registerController.js
@@ -5,14 +5,14 @@ const registerUser = async (req, res) => {
     const { username, password } = req.body
     const existingUser = await User.findOne({ username })
     if (existingUser) {
-      res.status(500).json({ "message": "User has been registered" })
+      return res.status(500).json({ "message": "User has been registered" })
     }
     const password_hash = (await (bcrypt.hash(password, 10))).toString()
     const saveUser = new User({
       username, password_hash
     })
     await saveUser.save()
-    res.json({ "message": "User saved successfully" })
+    return res.json({ "message": "User saved successfully" })
   } catch (error) {
     console.log(error.message)
   }
