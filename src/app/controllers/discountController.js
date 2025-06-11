56b78b0a2b2d5456bf23af3ae4485b056d1a465d
viethoang1520 <viethoang1520.dev@gmail.com>
2025-06-12 00:14:47 +0700

    feat: dicount API

--- src/app/controllers/discountController.js
+++ src/app/controllers/discountController.js
@@ -2,16 +2,17 @@ const User = require('../models/User')
 
 const applyDiscountForStudent = async (req, res) => {
   try {
-    const { userID, discount, expiryDate } = req.body
-    const user = await User.findById(userID)
+    const { user_id, discount, expiry_date } = req.body
+    const user = await User.findById(user_id)
+    console.log(user)
     if (!user) {
       return res.status(404).json({ "message": "User has not registered yet" })
     }
-    const updateUserDiscount = await User.updateOne({ _id: userID }, {
+    const updateUserDiscount = await User.updateOne({ _id: user_id }, {
       $set: {
         'passenger_categories.passenger_type': 'STUDENT',
         'passenger_categories.discount': discount,
-        'passenger_categories.expiry_date': expiryDate,
+        'passenger_categories.expiry_date': expiry_date,
       }
     })
     return res.json({ "message": "Student discount applied!!", updateUserDiscount })
@@ -21,19 +22,23 @@ const applyDiscountForStudent = async (req, res) => {
 }
 
 const applyFreeTicket = async (req, res) => {
-  const { userID, userType, expiryDate } = req.body
-  const user = await User.findById(userID)
-  if (!user) {
-    return res.status(404).json({ "message": "User not found" })
-  }
-  await User.updateOne({ _id: user._id }, {
-    $set: {
-      'passenger_categories.passenger_type': userType,
-      'passenger_categories.expiry_date': expiryDate || null,
+  try {
+    const { user_id, user_type, expiry_date } = req.body
+    const user = await User.findById(user_id)
+    if (!user) {
+      return res.status(404).json({ "message": "User not found" })
     }
-  })
+    await User.updateOne({ _id: user._id }, {
+      $set: {
+        'passenger_categories.passenger_type': user_type,
+        'passenger_categories.expiry_date': expiry_date || null,
+      }
+    })
 
-  return res.json({"message": "Apply free ticket sucessfully"})
+    return res.json({ "message": "Apply free ticket sucessfully" })
+  } catch (error) {
+    console.log(error.message)
+  }
 }
 
 
--- src/routes/discount.js
+++ src/routes/discount.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const { applyDiscountForStudent } = require('../app/controllers/discountController')
+const { applyDiscountForStudent, applyFreeTicket } = require('../app/controllers/discountController')
+
 router.post('/student', applyDiscountForStudent)
+router.post('/free', applyFreeTicket )
 
 module.exports = router
