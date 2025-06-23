e4170af9e1e5e2b39a0476719283ce64a1d71c09
nguyenbachduyhoang <nguyenduyhoang1494@gmail.com>
2025-06-23 13:20:41 +0700

    fix call api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
-const register = require('./register');
-const login = require('./login');
-const purchase = require('./purchase');
-const station = require('./station');
-const ticket = require('./ticket');
-const discount = require('./discount');
+const register = require('./register')
+const login = require('./login')
+const purchase = require('./purchase')
+const station = require('./station')
+const ticket = require('./ticket')
+const discount = require('./discount')
 
 function routes(app) {
   app.use('/login', login);
@@ -11,7 +11,7 @@ function routes(app) {
   app.use('/purchase', purchase);
   app.use('/station', station);
   app.use('/ticket', ticket);
-  app.use('/discount', discount);
+  app.use('/discount', discount)
 }
 
 module.exports = routes;
