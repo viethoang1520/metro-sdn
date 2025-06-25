407aad29d3bb6258daf4b7a3076ad7eac536e16c
nguyenbachduyhoang <nguyenduyhoang1494@gmail.com>
2025-06-25 23:11:14 +0700

    station controller

--- src/routes/station.js
+++ src/routes/station.js
@@ -1,14 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Station = require('../app/models/Station');
+const stationController = require('../app/controllers/stationController');
 
-router.get('/stations', async (req, res) => {
-  try {
-    const stations = await Station.find({});
-    res.json(stations);
-  } catch (err) {
-    res.status(500).json({ message: 'Server error' });
-  }
-});
+router.get('/', stationController.getAllStations);
 
 module.exports = router;
