395c5ebe0f8c5e9d54ddc2df249d804735ac5637
nguyenbachduyhoang <nguyenduyhoang1494@gmail.com>
2025-06-26 00:24:08 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-const register = require('./register')
-const login = require('./login')
-const purchase = require('./purchase')
-const station = require('./station')
-const ticket = require('./ticket')
-const discount = require('./discount')
-const schedule = require('./schedule')
-=======
 const register = require('./register');
 const login = require('./login');
 const purchase = require('./purchase');
@@ -15,7 +6,7 @@ const ticket = require('./ticket');
 const discount = require('./discount');
 const schedule = require('./schedule'); // giữ cái này từ develop
 
->>>>>>> 273b22e220582a15844c40ca76c0f818a2dfb3ff
+
 function routes(app) {
     app.use('/login', login);
     app.use('/register', register);
