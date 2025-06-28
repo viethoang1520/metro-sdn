a5fdbb4ac30e82a5583ac1a06d8a82197ba505bb
nhathao04 <nhathao06012004.bl@gmail.com>
2025-06-29 05:54:38 +0700

    fix

--- src/app/controllers/purchaseController.js
+++ src/app/controllers/purchaseController.js
@@ -27,7 +27,7 @@ const purchaseTicketsByType = async (req, res) => {
     if (
       user &&
       user.passenger_categories &&
-      user.passenger_categories.discount
+      user.passenger_categories.discount&& user.passenger_categories == "APPROVED"  
     ) {
       discount = user.passenger_categories.discount;
       discountMsg = ` (discount ${discount}% applied)`;
@@ -136,7 +136,7 @@ const purchaseTicketByRoute = async (req, res) => {
       if (
         user &&
         user.passenger_categories &&
-        user.passenger_categories.discount
+        user.passenger_categories.discount && user.passenger_categories.status === "APPROVED"
       ) {
         discount = user.passenger_categories.discount;
         route_price = route_price * (1 - discount / 100);
