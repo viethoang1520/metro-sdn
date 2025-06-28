ce864edf52afde3fbf2fda9400d3bd916a48c3af
viethoang1520 <viethoang1520.dev@gmail.com>
2025-06-29 03:10:34 +0700

    fix payment

--- src/app/controllers/PayOSController.js
+++ src/app/controllers/PayOSController.js
@@ -6,7 +6,7 @@ const Transaction = require('../models/Transaction')
 const createPayment = async (req, res) => {
   const orderCode = Number(String(new Date().getTime()).slice(-6))
   const user_id = req.id
-  const { items, transaction_id, total_price } = req.body 
+  const { items, transaction_id, total_price } = req.body
   const order = {
     orderCode,
     amount: total_price,
@@ -16,29 +16,48 @@ const createPayment = async (req, res) => {
     items
   };
   try {
-    if(!items || !transaction_id || !total_price){
+    if (!items || !transaction_id) {
+      console.log(items, transaction_id)
       return res.status(400).json({ error: 'Missing required fields' })
     }
     const transaction = await Transaction.findById(transaction_id)
     if (!transaction) {
       return res.status(404).json({ error: 'Transaction not found' })
     }
-    const paymentLink = await payOS.createPaymentLink(order);
-    const newOrder = new Order({
-      user_id,
-      order_code: orderCode,
-      order_date: new Date(),
-      description: `THANH TOAN VE ${orderCode}`,
-      transaction: transaction._id,
-      order_amount: total_price,
-      status: 'PENDING'
-    })
+    if (total_price <= 0) {
+      const newOrder = new Order({
+        user_id,
+        order_code: orderCode,
+        order_date: new Date(),
+        description: `THANH TOAN VE ${orderCode}`,
+        transaction: transaction._id,
+        order_amount: total_price,
+        status: 'PENDING'
+      })
+      await newOrder.save()
+      res.json({
+        checkoutUrl: `${process.env.CLIENT_URL}/payment/success?code=00&cancel=false&status=PAID&order_code=${orderCode}`,
+        qrCode: ``
+      });
+    } else {
+      const paymentLink = await payOS.createPaymentLink(order);
+      const newOrder = new Order({
+        user_id,
+        order_code: orderCode,
+        order_date: new Date(),
+        description: `THANH TOAN VE ${orderCode}`,
+        transaction: transaction._id,
+        order_amount: total_price,
+        status: 'PENDING'
+      })
 
-    await newOrder.save()
-    res.json({
-      checkoutUrl: paymentLink.checkoutUrl,
-      qrCode: paymentLink.qrCode
-    });
+      await newOrder.save()
+      res.json({
+        checkoutUrl: paymentLink.checkoutUrl,
+        qrCode: paymentLink.qrCode
+      });
+    }
+
   } catch (error) {
     console.error('Error creating payment link:', error);
     res.status(500).json({ error: error.message });
@@ -60,7 +79,7 @@ const receiveWebhook = async (req, res) => {
       order.status = 'PAID'
       transaction.status = 'PAID'
     } else {
-      order.status = 'CANCELLED' 
+      order.status = 'CANCELLED'
       transaction.status = 'CANCELLED'
     }
     await order.save()
