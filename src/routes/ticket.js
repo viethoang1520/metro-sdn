a8db6dfec23e7b9862c951eefaa51b244a1db738
nhathao04 <nhathao06012004.bl@gmail.com>
2025-07-10 20:00:05 +0700

    add route checkin, checkout

--- src/routes/ticket.js
+++ src/routes/ticket.js
@@ -7,4 +7,6 @@ router.get('/', ticketController.getAllTickets);
 router.get('/:id', ticketController.getTicketById);
 router.get('/user/:userId', ticketController.getAllTicketsByUserId);
 router.get('/active/:userId', ticketController.getActiveTicketsByUserId);
+router.post('/checkin', ticketController.checkIn);
+router.post('/checkout', ticketController.checkOut);
 module.exports = router;
