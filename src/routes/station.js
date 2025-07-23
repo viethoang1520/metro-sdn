046bdd84c0007b1a15a4b7f05d903da92e323bde
nhathao04 <nhathao06012004.bl@gmail.com>
2025-07-24 01:59:34 +0700

    fix: update station's status

--- src/routes/station.js
+++ src/routes/station.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const stationController = require('../app/controllers/stationController');
 
 router.get('/', stationController.getAllStations);
+router.get('/all', stationController.getActiveAndInactiveStations);
 router.post('/', stationController.createStation);
 router.put('/:id', stationController.updateStationStatusById);
 router.put('/', stationController.updateStationNameById);
